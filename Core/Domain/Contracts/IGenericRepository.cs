9ed3fb22453ba41631a42bc822e54832c4909ae5
admin <admin@DESKTOP-JKD834T>
2025-05-22 10:39:32 +0200

    Create IGeneric iRepository and its implementation(GenericRepository)

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+   public interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
+
+    {
+
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(TKey key);
+    }
+}
---
+++ Infrastucture/Persistance/Repositores/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Contracts;
+using Persistance.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositores
+{
+    internal class GenericRepository<TEntity, TKey>(StoreDbContext _dbcontext) : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+    {
+        public void Add(TEntity entity) => _dbcontext.Set<TEntity>().Add(entity);
+
+
+        public void Delete(TEntity entity) => _dbcontext.Set<TEntity>().Remove(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        => await _dbcontext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity> GetAsync(TKey key)
+            => await _dbcontext.Set<TEntity>().FindAsync(key);
+
+
+        public void Update(TEntity entity) => _dbcontext.Set<TEntity>().Update(entity);
+
+        
+    }
+}
